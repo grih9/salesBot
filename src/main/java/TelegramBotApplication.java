06aa506264b10a038d826557f2cf281be8f356bd
Katerina <zaitceva_ek@mail.ru>
2021-12-16 01:00:45 +0300

    #38 parser for Diksi

--- src/main/java/TelegramBotApplication.java
+++ src/main/java/TelegramBotApplication.java
@@ -1,9 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+
+import bot.Item;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 import bot.TelegramBot;
+import database.City;
+import database.Shop;
+import parser.Parser;
 
 public class TelegramBotApplication {
     private static final Map<String, String> getenv = System.getenv();
