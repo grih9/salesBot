7d42bd8b28d2791a2bf3d026adfa9516557defa3
AlinaShestakova <shestakova.ayu@edu.spbstu.ru>
2021-12-20 22:02:02 +0300

    Fix

--- src/main/java/parser/WebDriverSingleton.java
+++ src/main/java/parser/WebDriverSingleton.java
@@ -1,5 +1,8 @@
 package parser;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -20,7 +23,9 @@ public class WebDriverSingleton {
             options.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
             options.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
             options.addArguments("--headless");
+            options.addArguments("--ignore-certificate-errors");
             options.addArguments("window-size=1800x900");
+            options.addArguments("--disable-dev-shm-usage");
             String userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36";
             options.addArguments("--user-agent=" + userAgent);
             driver = new ChromeDriver(options);
