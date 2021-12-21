98808f2a6e5adcc65bf82ad10f2b2b72fa55b0f4
Denis Korolev <denikorol@mail.ru>
2021-12-21 22:50:52 +0300

    update validation method

--- src/main/java/bot/NonCommand.java
+++ src/main/java/bot/NonCommand.java
@@ -12,18 +12,8 @@ import java.util.stream.IntStream;
  */
 public class NonCommand {
     public Boolean checkValid(String text) {
-        text = text.replaceAll(" ", "");
-        text = text.replaceAll("\\t", "");
-        text = text.replaceAll("\\s", "");
-        //Pattern pattern = Pattern.compile("((,*[ ]*)*[0-9]+(,*[ ]*)*)+");
-        Pattern pattern = Pattern.compile("[,]*[0-9]+");
+        Pattern pattern = Pattern.compile("((,*[ ]*)*[0-9]+(,*[ ]*)*)+");
         System.out.println(pattern.matcher(text).pattern());
-
-        Matcher m = pattern.matcher(text);
-        if(m.find())
-        {
-            System.out.println("TAG " + m.group());
-        }
 
         return pattern.matcher(text).matches();
     }
